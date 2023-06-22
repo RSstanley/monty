5955ed463cdbaa89efdd37bb984e69866a0aa742
HoOdpHarMxcisT <122929857+HoOdpHarMxcisT@users.noreply.github.com>
2023-06-22 15:16:10 +0200

    Update pstr.c

--- pstr.c
+++ pstr.c
@@ -3,9 +3,6 @@
 
 /**
  * pstr - prints the string starting at top of the stack
- * @stack: head of stack
- * @ln: line number, unused
- *
  * Description: int stored in each element of stack is
  * treated as a character, stops printing when stack
  * is over, element is 0 or not in ascii table
